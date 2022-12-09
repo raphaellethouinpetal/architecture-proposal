bd979887617025de287257b720bdc6410f7a9ba7
Raphaëlle Thouin <rthouin@petalmd.com>
2022-12-09 10:24:01 -0500

    add web

---
+++ Web/src/Xacte.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Xacte.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
